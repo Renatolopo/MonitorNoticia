610d3ed95dcab19363a5821006f8d055406da694
Renatolopo62 <renatolopo62@gmail.com>
2020-07-04 11:15:51 -0300

    coleta dados do site G1

---
+++ scripits/G1.py
@@ -0,0 +1,53 @@
+import feedparser
+from time import sleep
+import pandas as pd
+
+#retorna a linha com uma noticia e remove os caracteres ", ', e \n.
+def getEntry(entry):
+	row =  [f'{entry.title}',f'|{entry.title_detail}',f'|{entry.links}',f'|{entry.link}'\
+	,f'|{entry.id}',f'|{entry.guidislink}'f'|{entry.summary}',f'|{entry.summary_detail}'\
+	,f'|{entry.tags}',f'|{entry.published}',f'|{entry.published_parsed}']
+	for i in range(0,len(row)):
+		row[i] =  row[i].replace("'", " ")
+		row[i] =  row[i].replace('"', " ")
+		row[i] =  row[i].replace('\n', " ")
+	return row
+
+def criaArquivo():
+	fl = open('../output/G1.csv','w')
+	fl.write('title |title_detail |links |link |id |guidislink |summary |summary_detail \
+		|tags |published |published_parsed\n')
+	fl.close()
+#escreve uma linha no arquivo.
+def setArquivo(row):
+	fl = open('../output/G1.csv','a')
+	for i in row:
+		fl.write(i)
+	fl.write('\n')
+	fl.close()
+
+#verifica se o titulo já existe no arquivo para não ter repetição.
+def titleInFile(title):
+	file = pd.read_csv('../output/G1.csv','r',delimiter='|')
+	file = file.values
+	for i in file:
+		if title in i:
+			return True
+	return False
+
+criaArquivo()
+cont = 0
+while True:
+	#feed RSS do site G1.
+	NewsFeed = feedparser.parse("http://g1.globo.com/dynamo/rss2.xml")
+	for entry in NewsFeed.entries:
+		row = getEntry(entry)
+		title = row[0]
+		if titleInFile(title):
+			continue
+		else:
+			setArquivo(row)
+			print(f'linha {cont} adicionada')
+			cont += 1
+	sleep(60)
+
