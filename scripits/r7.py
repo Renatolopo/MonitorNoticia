dc65f010f9d53ce2ac98cd96034e00a10984c5ad
Renatolopo62 <renatolopo62@gmail.com>
2020-07-09 16:33:04 -0300

    configurado para o banco de dados

--- scripits/r7.py
+++ scripits/r7.py
@@ -1,55 +1,26 @@
 import feedparser
+import requests
+import MySQLdb
 from time import sleep
-import pandas as pd
 
-#retorna a linha com uma noticia e remove os caracteres ", ', e \n.
 def getEntry(entry):
-	row = [f'{entry.id}',f'|{entry.guidislink}',f'|{entry.link}',f'|{entry.published}',f'|{entry.published_parsed}',f'|{entry.updated}'\
-	,f'|{entry.updated_parsed}',f'|{entry.links}',f'|{entry.href}',f'|{entry.title}',f'|{entry.title_detail}',f'|{entry.summary}'\
-	,f'|{entry.summary_detail}',f'|{entry.content}',f'|{entry.mediaurl}',f'|{entry.mediasource}',f'|{entry.authors}'\
-	,f'|{entry.author_detail}',f'|{entry.author}']
-	for i in range(0, len(row)):
-		row[i] =  row[i].replace("'", " ")
-		row[i] =  row[i].replace('"', " ")
-		row[i] =  row[i].replace('\n', " ")
+	row =  [entry.title, entry.link, entry.summary, entry.published]
 	return row
 
-def criaArquivo():
-	fl = open('../output/r7.csv','w')
-	fl.write('id |guidislink |link |published |published_parsed |updated |updated_parsed |links |href |title |title_detail \
-		|summary |summary_detail |content |mediaurl |mediasource |authors |author_detail |author\n')
-	fl.close()
-
-#escreve uma linha no arquivo.
-def setArquivo(row):
-	fl = open('../output/r7.csv', 'a')
-	for i in row:
-		fl.write(i)
-	fl.write('\n')
-	fl.close()
-
-#verifica se o titulo já existe no arquivo para não repetir uma mesma noticia.
-def titleInFile(title):
-	file = pd.read_csv('../output/r7.csv','r',delimiter='|')
-	file = file.values
-	for i in file:
-		if title in i:
-			return True
-	return False
-
-
-criaArquivo()
-cont = 0
 while True:
-	#feed RSS do site R7.
+	con = MySQLdb.connect(host="servidor", user="usuario", passwd="senha", db="base de dados")
+	con.select_db('base de dados')
+	cursor = con.cursor()
+
 	NewsFeed = feedparser.parse("https://noticias.r7.com/feed.xml")
 	for entry in NewsFeed.entries:
 		row = getEntry(entry)
-		title = row[9].replace('|','')
-		if titleInFile(title):
+		try:
+			cursor.execute('INSERT INTO R7 (TITLE, LINK, SUMMARY, PUBLISHED)\
+			 VALUES (%s,%s,%s,%s)',(row[0], row[1], row[2], row[3]))
+			print('adicionado com sucesso')
+		except:
 			continue
-		else:
-			setArquivo(row)
-			print(f'linha {cont} adicionada')
-			cont += 1
-	sleep(60)
+	con.commit()
+	con.close()
+	sleep(120)
--- scripits/veja.py
+++ scripits/veja.py
@@ -1,59 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+import MySQLdb
 from time import sleep
 
-#retorna a linha com uma noticia e remove os caracteres ", ', e \n.
 def getNoticia(noticia):
 	titulo = noticia.h2.text
 	descricao = noticia.find('span', class_ = 'description').text
 	descricao = descricao.replace('\t','').replace('\n','')
 	data = noticia.time.text.replace('| ','')
+	
+	titulo = str(titulo)
+	data = str(data)
+	descricao = str(descricao)
+	if not descricao:
+		descricao = '-' 
 
-	row = [f'{titulo}',f'|{descricao}',f'|{data}']
-	for i in range(0,len(row)):
-		row[i] =  row[i].replace("'", " ")
-		row[i] =  row[i].replace('"', " ")
-		row[i] =  row[i].replace('\n', " ")
-	return row
+	return [titulo, descricao, data]
 
-def createFile():
-	fl = open('../output/veja.csv','w')
-	fl.write('Titulo |Descrição |Data\n')
-	fl.close()
-#escreve uma linha no arquivo
-def setFile(row):
-	fl = open('../output/veja.csv','a')
-	for i in row:
-		fl.write(i)
-	fl.write('\n')
-	fl.close()
-
-#verifica se o titulo já existe no arquivo para não háver repetições.
-def titleInFile(title):
-	file = pd.read_csv('../output/veja.csv','r', delimiter = '|')
-	file = file.values
-	for i in file:
-		if title in i:
-			return True
-	return False
-
-createFile()
-cont=0
 while True:
+	con = MySQLdb.connect(host="servidor", user="usuario", passwd="senha", db="base de dados")
+	con.select_db('base de dados')
+	cursor = con.cursor()
+
 	r = requests.get('https://veja.abril.com.br/blog/')
 	soup = BeautifulSoup(r.content, 'html.parser')
 	noticias = soup.find_all('div', attrs={'class':'card'})
+
 	for noticia in noticias:
 		row = getNoticia(noticia)
-		title = row[0]
-		if titleInFile(title):
+		try:
+			cursor.execute('INSERT INTO veja (TITULO, DESCRICAO, HORARIO)\
+			 VALUES (%s,%s,%s)',(row[0], row[1], row[2]))
+			print('adicionado com sucesso')
+		except:
 			continue
-		else:
-			setFile(row)
-			print(f'linha {cont} adicionada')
-			cont += 1
-	sleep(60)
-
-
-
+	con.commit()
+	con.close()
+	sleep(120)
