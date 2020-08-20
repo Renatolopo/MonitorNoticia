958c21d9777398e70b60bedb24753562706b0105
Renato Lopo <renatolopo62@gmail.com>
2020-08-20 18:52:58 -0300

    mudado para biblioteca mysql.connector

--- scripts/coleta_das_bases_de_dados.py
+++ scripts/coleta_das_bases_de_dados.py
@@ -1,17 +1,27 @@
 import pandas as pd
-import sqlalchemy
+import mysql.connector
 
-def getConexao(drive, user, passwd, host, porta, db):
-	return  sqlalchemy.create_engine(f'mysql+{drive}://{user}:{passwd}@{host}:{porta}/{db}')
+def getConexao():
+	return  mysql.connector.connect(
+        user="xx",
+        host="xx",
+        database="xx",
+        password='xx',
+    )
 
-def getDataBase(data_base, engine):
-	return pd.read_sql_table(data_base,engine)
+
+def getDataBase(data_base, con):
+	return pd.read_sql(f'select * from {data_base};', con=con) 
 	 
 
-engine = getConexao('xx', 'xx', 'xx', 'xx', 'xx', 'xx')
-list_db = ['G1', 'Sbt', 'veja', 'R7', 'Folha', 'Twitter1']
+con = getConexao()
+list_db = ['G1', 'Sbt', 'veja', 'R7', 'Folha', 'Twitter']
 df = dict()
 for db in list_db:
-	df[db] = getDataBase(db, engine)
+	df[db] = getDataBase(db, con)
 
 print(df['Twitter1'])
+
+con.close()
+
+
