40a10d8e530d343cf13eaa1b3f70364887136e0b
Renato Lopo <renatolopo62@gmail.com>
2020-11-03 07:16:00 -0300

    streaming do twitter

---
+++ scripts/streaming_twitter.py
@@ -0,0 +1,62 @@
+import tweepy
+import pymysql as MySQLdb
+from time import sleep
+
+# Sobre escrevendo tweepy.StreamListener
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        #print(status.text)
+        set_database(status)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return False
+
+# Inserir no banco de dados
+def set_database(tweet):
+	# cria conexão com o banco de dados
+	con = MySQLdb.connect(host="200.131.5.71", user="renato", passwd="renato20", db="Monitor_de_noticia")
+	cursor = con.cursor()
+
+	try: 
+		cursor.execute('INSERT INTO Twitter_streaming (USUARIO, TWEET, DATA_TWEET)\
+			VALUES(%s, %s, %s)',(tweet.user.screen_name, tweet.text, tweet.created_at))
+		print("Adicionado.....")
+	except:
+		# essa exceção acontece caso o tweet já exista na base de dados
+		print('NÂO ADICIONADO*********')
+	
+	con.commit()
+	con.close()
+
+
+
+# credenciais
+chave_consumidor = 'uvYDBa4HA6xSbGDVVLuL5kWWW'
+segredo_consumidor = 'RGyzZdfEjdMSm87109yiMyPqkaLYEN1ox0ogOQSaxENyU9ziq6'
+token_acesso = '2559540815-3OWEk9l7ImzsH9UP4ZVZ6aKgCwV94s6EdaodlQC'
+token_acesso_segredo = 'p6eZ7banSAdSt3mDd5TxyVLwD7me66HDSxGhyQjW2Iqk0'
+
+# criando uma autenticação
+autenticacao = tweepy.OAuthHandler(chave_consumidor, segredo_consumidor)
+autenticacao.set_access_token(token_acesso, token_acesso_segredo)
+
+
+# Instanciando MyStreamListener
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = autenticacao, listener=myStreamListener)
+
+# cordenadas do Brasil
+location = [-73.09,-32.72,-33.98,4.09]
+
+while True:
+	try:
+		myStream.filter( locations= location)
+		print('Limite da API excedido')
+		sleep(60 *20)	
+	except:
+		print('Erro aqui')
+
+	
