9b909e670385574a267daa83650bcd132f5adc71
Renatolopo <renatolopo62@gmail.com>
2021-02-20 10:08:16 -0300

    arrumando repositorio

--- scripts/twitter/conexao.py
+++ scripts/twitter/conexao.py
@@ -3,10 +3,10 @@ import pymysql as MySQLdb
 
 
 def get_api(stream=False):
-	chave_consumidor = 'uvYDBa4HA6xSbGDVVLuL5kWWW'
-	segredo_consumidor = 'RGyzZdfEjdMSm87109yiMyPqkaLYEN1ox0ogOQSaxENyU9ziq6'
-	token_acesso = '2559540815-3OWEk9l7ImzsH9UP4ZVZ6aKgCwV94s6EdaodlQC'
-	token_acesso_segredo = 'p6eZ7banSAdSt3mDd5TxyVLwD7me66HDSxGhyQjW2Iqk0'
+	chave_consumidor = 'x'
+	segredo_consumidor = 'x'
+	token_acesso = 'x-x'
+	token_acesso_segredo = 'x'
 
 	autenticacao = tweepy.OAuthHandler(chave_consumidor, segredo_consumidor)
 	autenticacao.set_access_token(token_acesso, token_acesso_segredo)
@@ -18,8 +18,7 @@ def get_api(stream=False):
 def get_mysql():
 	con = MySQLdb.connect(host='localhost',
                          user='root',
-                         password='renato20',
+                         password='',
                          db='test')
 	return con
 
-print('Hello world')
